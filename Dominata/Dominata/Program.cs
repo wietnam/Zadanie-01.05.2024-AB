4191eb40cabfaa9b9ef0d3b8e342dd4b7e9608c6
wietnam <145010469+wietnam@users.noreply.github.com>
2024-05-15 10:05:58 +0200

    Add files via upload

---
+++ Dominata/Dominata/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DominantaListy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rozmiar listy:");
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> lista = new List<int>();
+            Random rand = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                lista.Add(rand.Next(-99, 100));
+            }
+
+            Console.WriteLine("Lista:");
+            foreach (var num in lista)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            int dominanta = lista[0];
+            int maxCount = 0;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                int currentCount = 0;
+                for (int j = 0; j < lista.Count; j++)
+                {
+                    if (lista[j] == lista[i])
+                    {
+                        currentCount++;
+                    }
+                }
+
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                    dominanta = lista[i];
+                }
+            }
+
+            Console.WriteLine($"Najczęściej występująca wartość (dominanta): {dominanta}");
+            Console.WriteLine($"Liczba wystąpień dominanty: {maxCount}");
+        }
+    }
+}
